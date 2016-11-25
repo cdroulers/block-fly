4dd9cdc2e48fa9160957d5a48eb68b55bbc09246
Olivier Lafleur <olivier.lafleur@cll.qc.ca>
2016-11-25 14:19:00 -0500

    Fix lint

--- src/block-fly/app.ts
+++ src/block-fly/app.ts
@@ -9,54 +9,56 @@ const canvas = document.getElementById("root") as HTMLCanvasElement;
 
 const parser = new BoardParser();
 
-getLevels().then(function(response) {
-  const levels = response;
+getLevels().then(
+  function(response: string[]): void {
+    const levels = response;
 
-  const levelSet = new LevelSet(levels, parser);
+    const levelSet = new LevelSet(levels, parser);
 
-  levelSet.onLevelFinished = () => {
-    alert("YOU WIN THIS LEVEL. Give yourself a high-five");
-    levelSet.nextLevel();
+    levelSet.onLevelFinished = () => {
+      alert("YOU WIN THIS LEVEL. Give yourself a high-five");
+      levelSet.nextLevel();
+      writeToCanvas(canvas, levelSet.currentLevel);
+    };
+
+    levelSet.onSetFinished = () => {
+      alert("YOU Finished all levels. Sweet Christmas!");
+    };
+
     writeToCanvas(canvas, levelSet.currentLevel);
-  };
 
-  levelSet.onSetFinished = () => {
-    alert("YOU Finished all levels. Sweet Christmas!");
-  };
-
-  writeToCanvas(canvas, levelSet.currentLevel);
-
-  bindDefaultControls(canvas, levelSet);
-  bindMobileControls(canvas, levelSet);
-}, function(Error) {
-  console.log(Error);
-});
+    bindDefaultControls(canvas, levelSet);
+    bindMobileControls(canvas, levelSet);
+  },
+  function(error: string): void {
+    console.log(error);
+  });
 
 function getLevels(): Promise<string[]> {
-  return new Promise<string[]>(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
+  return new Promise<string[]>(function(resolve: any, reject: any): void {
+    let xhr = new XMLHttpRequest();
 
-    xhr.open('GET', '/assets/default-levels.json');
-    xhr.responseType = 'json';
+    xhr.open("GET", "/assets/default-levels.json");
+    xhr.responseType = "json";
 
-    xhr.onload = function() {
-      var levels: string[] = [];
+    xhr.onload = function(): any {
+      let levels: string[] = [];
 
-      if (xhr.status == 200) {
-        for (var level in xhr.response) {
-          levels.push(xhr.response[level].join("\n"));
+      if (xhr.status === 200) {
+        for (let i = 0 ; i < xhr.response.length ; i++) {
+          levels.push(xhr.response[i].join("\n"));
         }
 
         resolve(levels);
       } else {
-        reject(Error('Cannot load levels; error :' + xhr.statusText));
+        reject(Error("Cannot load levels; error :" + xhr.statusText));
       }
-    }
+    };
 
-    xhr.onerror = function() {
-      reject(Error('There was a network error.'));
-    }
+    xhr.onerror = function(): any {
+      reject(Error("There was a network error."));
+    };
 
     xhr.send();
-  })
+  });
 }
