2620c38ec613d6cede8773e6d46406e88069c3f3
Olivier Lafleur <olivier.lafleur@cll.qc.ca>
2016-11-25 13:37:01 -0500

    Extract levels to JSON

--- src/block-fly/app.ts
+++ src/block-fly/app.ts
@@ -3,196 +3,60 @@ import LevelSet from "./game/levelSet";
 import { writeToCanvas } from "./display/canvasDisplay";
 import { bindDefaultControls } from "./display/defaultControls";
 import { bindMobileControls } from "./display/mobileControls";
+import * as Promise from "bluebird";
 
 const canvas = document.getElementById("root") as HTMLCanvasElement;
 
 const parser = new BoardParser();
 
-const levels = getLevels();
-const levelSet = new LevelSet(levels, parser);
+getLevels().then(function(response) {
+  const levels = response;
 
-levelSet.onLevelFinished = () => {
-  alert("YOU WIN THIS LEVEL. Give yourself a high-five");
-  levelSet.nextLevel();
+  const levelSet = new LevelSet(levels, parser);
+
+  levelSet.onLevelFinished = () => {
+    alert("YOU WIN THIS LEVEL. Give yourself a high-five");
+    levelSet.nextLevel();
+    writeToCanvas(canvas, levelSet.currentLevel);
+  };
+
+  levelSet.onSetFinished = () => {
+    alert("YOU Finished all levels. Sweet Christmas!");
+  };
+
   writeToCanvas(canvas, levelSet.currentLevel);
-};
 
-levelSet.onSetFinished = () => {
-  alert("YOU Finished all levels. Sweet Christmas!");
-};
+  bindDefaultControls(canvas, levelSet);
+  bindMobileControls(canvas, levelSet);
+}, function(Error) {
+  console.log(Error);
+});
 
-writeToCanvas(canvas, levelSet.currentLevel);
+function getLevels(): Promise<string[]> {
+  return new Promise<string[]>(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
 
-bindDefaultControls(canvas, levelSet);
-bindMobileControls(canvas, levelSet);
+    xhr.open('GET', '/assets/default-levels.json');
+    xhr.responseType = 'json';
 
-function getLevels(): string[] {
-  return [
-`
-#                  #
-#                  #
-#                  #
-#   #       #      #
-#D  #   # B # B P  #
-####################`,
+    xhr.onload = function() {
+      var levels: string[] = [];
 
-" #    ##        ##    \n" +
-" #                #   \n" +
-"##                 #  \n" +
-"#D                  # \n" +
-"##                   #\n" +
-" #           #  B    #\n" +
-" #           #B BBP  #\n" +
-" #####   #############\n" +
-"     #  B#            \n" +
-"     #####            ",
+      if (xhr.status == 200) {
+        for (var level in xhr.response) {
+          levels.push(xhr.response[level].join("\n"));
+        }
 
-" #                 \n" +
-" #   ############# \n" +
-"# # #             #\n" +
-"#  #              #\n" +
-"#                B#\n" +
-"#               BB#\n" +
-"# ###    P   #B ## \n" +
-"# # #    #  #####  \n" +
-"# # #BB ##  #      \n" +
-"#D# ###### ##      \n" +
-"### ##   ###       ",
+        resolve(levels);
+      } else {
+        reject(Error('Cannot load levels; error :' + xhr.statusText));
+      }
+    }
 
-"                  #     \n" +
-"                 # #    \n" +
-"       #        #   #   \n" +
-"      # #      #     #  \n" +
-"   ###   #    #       # \n" +
-"  #       #  #         #\n" +
-" #         ##          #\n" +
-" #                    B#\n" +
-" #                   BB#\n" +
-" #               P   ###\n" +
-"##    #          #   #  \n" +
-"#D    # B        #####  \n" +
-"##### # B   B  ###      \n" +
-"    # # B # #B #        \n" +
-"    # ##########        \n" +
-"    ###                 ",
+    xhr.onerror = function() {
+      reject(Error('There was a network error.'));
+    }
 
-"     ###    ######### \n" +
-" ####   ####         #\n" +
-"#                    #\n" +
-"#                    #\n" +
-"#                    #\n" +
-"#     #              #\n" +
-"#     #              #\n" +
-"#     #BBBB          #\n" +
-"#D   #######P        #\n" +
-"## ###     ## #     B#\n" +
-" # #        # ##   BB#\n" +
-" # #        # ##  BBB#\n" +
-" ###        # ########\n" +
-"            ###       ",
-
-" ###             ####\n" +
-" #  #############   #\n" +
-"##                  #\n" +
-"#D                  #\n" +
-"##                  #\n" +
-" #                BB#\n" +
-" #BB        #  B  ###\n" +
-" #BBB       #PBBB #  \n" +
-" #BBBB      ##### #  \n" +
-" #####    ###   ###  \n" +
-"     #   B#          \n" +
-"     ## ###          \n" +
-"      ###            ",
-
-"  #   #####   ##   ###  \n" +
-" # # #     # #  # #   # \n" +
-" #  ##      ##   ##    #\n" +
-" #   #       #    #    #\n" +
-" #                    B#\n" +
-" #                    B#\n" +
-"##                   BB#\n" +
-"#D   B               ###\n" +
-"##   # B     #    ## #  \n" +
-" #   # B    ## B P####  \n" +
-" ##  # BBB  ## BBB#     \n" +
-"  #  ###### #######     \n" +
-"  ## #    ###           \n" +
-"   ###                  ",
-
-" ###       ####   #######  \n" +
-"#   #     #    # #       # \n" +
-"#    #   #     ##         #\n" +
-"#B    ###    # #     ###  #\n" +
-"#BB         ##      ## #  #\n" +
-"####       ##          #D #\n" +
-"   ##            ##    ## #\n" +
-"  #    B #      #  #      #\n" +
-"  #    B# #    #   #      #\n" +
-" #   ###   #    #  #     B#\n" +
-" #      # #      ##     BB#\n" +
-"#        #           ######\n" +
-"#            B            #\n" +
-"#    B      ###          B#\n" +
-"#   ###                 BB#\n" +
-"#        B       B  P  BBB#\n" +
-"###########################",
-
-"        ###         \n" +
-"       #   #        \n" +
-"      #     #  #####\n" +
-"     #       ##    #\n" +
-"    #     B        #\n" +
-"   #      BB      B#\n" +
-"  #       ###    BB#\n" +
-" #            P ####\n" +
-"#             B    #\n" +
-"#D           ###   #\n" +
-"##    ##   #      B#\n" +
-" #    ##B  ##   ####\n" +
-" #    #######  ##   \n" +
-" ###  #     # ##    \n" +
-"   # ##     ###     \n" +
-"   ###              ",
-
-"   #####################   \n" +
-" ##           #         #  \n" +
-"####B       BB#B   BBB B## \n" +
-"#  ##  #   #####  B### ## #\n" +
-"#   #  ##        ### ###  #\n" +
-"#   ##  ##BBBB            #\n" +
-"#D       #######          #\n" +
-"##        #   ###        ##\n" +
-" #     B   # #  ##        #\n" +
-" #     #    #    ##       #\n" +
-" ####  ##             #####\n" +
-"   #####      P           #\n" +
-"   #          #           #\n" +
-"   #         ##    ########\n" +
-"   #        ##           # \n" +
-"   #          B         B# \n" +
-"   #B    ###########   BB# \n" +
-"   #BB  ##         ## BBB# \n" +
-"   ######           ###### ",
-
-"#############################\n" +
-"#  #   #                    #\n" +
-"#     B#BB            ##### #\n" +
-"#B   ### B##     B  ##  D # #\n" +
-"#BB    ###   P  B       # # #\n" +
-"###  BB#     # B          # #\n" +
-"#   ####      #  ###   ###  #\n" +
-"#B            # #      #  B #\n" +
-"#BB       ### # #B    #  ####\n" +
-"#### B   ###  # ##B  # B #  #\n" +
-"#           B ###  B#   #   #\n" +
-"#   B     BB #   ####       #\n" +
-"#    #########        ##### #\n" +
-"#              B   B##    # #\n" +
-"####           B   #    BB# #\n" +
-"#B##   #    #          #### #\n" +
-"##B### #    #   BBB B       #\n" +
-"#B#B#B##    #        BBB    #\n" +
-"#############################"
-  ];
+    xhr.send();
+  })
 }
