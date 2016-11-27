f4abbe60f1b5e17321cb08462180b7eeaa48d280
Olivier Lafleur <olivier.lafleur@vooban.com>
2016-11-26 22:15:20 -0500

    Complete different loading possibilities

--- src/block-fly/app.ts
+++ src/block-fly/app.ts
@@ -9,52 +9,74 @@ const canvas = document.getElementById("root") as HTMLCanvasElement;
 
 const parser = new BoardParser();
 
-initLevels();
-
-function initLevels(path: string = "assets/default-levels.json"): void {
-  getLevels(path).then((response: string[]) => {
-      const levels = response;
-
-      const levelSet = new LevelSet(levels, parser);
-
-      levelSet.onLevelFinished = () => {
-        alert("YOU WIN THIS LEVEL. Give yourself a high-five");
-        levelSet.nextLevel();
-        writeToCanvas(canvas, levelSet.currentLevel);
-      };
-
-      levelSet.onSetFinished = () => {
-        alert("YOU Finished all levels. Sweet Christmas!");
-      };
-
-      writeToCanvas(canvas, levelSet.currentLevel);
-
-      bindDefaultControls(canvas, levelSet);
-      bindMobileControls(canvas, levelSet);
-
-      return response;
-    }).catch((error: string): void => {
-      alert("Unable to find levels");
-    });
-}
+initDefaultLevels();
 
 let loadRemoteButton = document.getElementById("load-remote");
 loadRemoteButton.addEventListener("click", function(): void {
     let url = (document.getElementById("remote-path") as HTMLInputElement).value;
-    initLevels(url);
+
+    if (url !== "") {
+      getXHRLevels(url).then(initGame)
+      .catch((error: string): void => {
+        alert("Unable to find levels");
+      });
+    } else {
+      alert("Please enter an URL");
+    }
+
 });
 
 let loadDefaultsButton = document.getElementById("load-defaults");
 loadDefaultsButton.addEventListener("click", function(): void {
-    initLevels();
+    initDefaultLevels();
 });
 
 let loadLocalLevels = document.getElementById("local-levels");
 loadLocalLevels.addEventListener("change", function(): void {
-    console.log((loadLocalLevels as HTMLInputElement).files[0]);
+    let levelsFile = (loadLocalLevels as HTMLInputElement).files[0];
+
+    let reader = new FileReader();
+    reader.onload = e => initGame(transformReponseToLevels(JSON.parse(reader.result)));
+    reader.readAsText(levelsFile);
 });
 
-function getLevels(path: string): Promise<string[]> {
+function initDefaultLevels(): void {
+  getXHRLevels("assets/default-levels.json").then(initGame)
+  .catch((error: string): void => {
+    alert("Unable to find levels");
+  });
+}
+
+function initGame(levels: string[]): void {
+  const levelSet = new LevelSet(levels, parser);
+
+  levelSet.onLevelFinished = () => {
+    alert("YOU WIN THIS LEVEL. Give yourself a high-five");
+    levelSet.nextLevel();
+    writeToCanvas(canvas, levelSet.currentLevel);
+  };
+
+  levelSet.onSetFinished = () => {
+    alert("YOU Finished all levels. Sweet Christmas!");
+  };
+
+  writeToCanvas(canvas, levelSet.currentLevel);
+
+  bindDefaultControls(canvas, levelSet);
+  bindMobileControls(canvas, levelSet);
+}
+
+function transformReponseToLevels(response: string[][]): string[] {
+  let levels: string[] = [];
+
+  for (let i = 0 ; i < response.length ; i++) {
+    levels.push(response[i].join("\n"));
+  }
+
+  return levels;
+}
+
+function getXHRLevels(path: string): Promise<string[]> {
   return new Promise<string[]>(function(resolve: any, reject: any): void {
     let xhr = new XMLHttpRequest();
 
@@ -62,14 +84,8 @@ function getLevels(path: string): Promise<string[]> {
     xhr.responseType = "json";
 
     xhr.onload = function(): any {
-      let levels: string[] = [];
-
       if (xhr.status === 200) {
-        for (let i = 0 ; i < xhr.response.length ; i++) {
-          levels.push(xhr.response[i].join("\n"));
-        }
-
-        resolve(levels);
+        resolve(transformReponseToLevels(xhr.response));
       } else {
         reject(Error("Cannot load levels; error :" + xhr.statusText));
       }
