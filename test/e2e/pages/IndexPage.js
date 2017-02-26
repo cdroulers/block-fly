67a56f99020ea92e42bd2b9f236a2b00ad47b877
Christian Droulers <droulers.christian@gmail.com>
2017-02-26 08:39:35 -0500

    Load remote levels

--- test/e2e/pages/IndexPage.js
+++ test/e2e/pages/IndexPage.js
@@ -19,6 +19,26 @@ class EnterPasswordDialog extends Dialog {
   }
 }
 
+class RemoteLevelsDialog extends Dialog {
+  constructor (selector) {
+    super(selector);
+
+    this.url = this.content.find("#remote-levels");
+    this.submit   = this.actions.find("button[type='submit']");
+    this.cancel   = this.actions.find("button.close");
+  }
+}
+
+class LocalLevelsDialog extends Dialog {
+  constructor (selector) {
+    super(selector);
+
+    this.url = this.content.find("#local-levels");
+    this.submit   = this.actions.find("button[type='submit']");
+    this.cancel   = this.actions.find("button.close");
+  }
+}
+
 class MessageBar {
   constructor () {
     this.container = Selector("#message");
@@ -32,11 +52,15 @@ export default class IndexPage {
     this.menu   = {
       enterPassword: Selector(".mdl-navigation__link").withText("Enter a password"),
       loadChildrensLevels: Selector(".mdl-navigation__link").withText("Load children's tutorial levels"),
-      loadDefaultLevels: Selector(".mdl-navigation__link").withText("Load default levels")
+      loadDefaultLevels: Selector(".mdl-navigation__link").withText("Load default levels"),
+      loadRemoteLevels: Selector(".mdl-navigation__link").withText("Load remote levels"),
+      loadLocalLevels: Selector(".mdl-navigation__link").withText("Load local levels")
     };
 
     this.dialogs = {
-      enterPassword: new EnterPasswordDialog("#enter-password")
+      enterPassword: new EnterPasswordDialog("#enter-password"),
+      remoteLevels: new RemoteLevelsDialog("#load-remote-file-dialog"),
+      localLevels: new LocalLevelsDialog("#load-local-file-dialog")
     };
 
     this.messageBar = new MessageBar();
